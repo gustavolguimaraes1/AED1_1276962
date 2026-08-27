8b1bb90fa832d095fa42454927b4e7c83699c553
Gustavo Lima Guimarães <gustavolguimaraes2109@gmail.com>
2026-08-27 09:19:35 -0300

    Update 1383b.c

--- 1383b.c
+++ 1383b.c
@@ -1,1 +1,10 @@
-
+/* -----------------------------------------------------------------------
+Disciplina  : Algoritmo e Estrutura de Dados 2026S1
+Nome        : Gustavo Lima Guimarães
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1383
+Data        : 27/08/2026
+Objetivo    : Verificar se o sudoku está corretamente preenchido usando alocação dinâmica de matrizes.
+Dificuldade : Alta.
+Uso de IA   : Para auxiliar na solução de verificar os "mini quadrantes" (3x3).
+------------------------------------------------------------------------- */
