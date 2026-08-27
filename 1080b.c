80cb31621ddfcbb38764cebef57b71975c31b074
Gustavo Lima Guimarães <gustavolguimaraes2109@gmail.com>
2026-08-27 08:48:45 -0300

    Achar o maior valor do vetor e seu indice com alocação dinâmica de vetores.
    The program reads 100 integer values, finds the largest value, and its position using dynamic memory allocation.

--- 1080b.c
+++ 1080b.c
@@ -1,1 +1,45 @@
+/* -----------------------------------------------------------------------
+Disciplina  : Algoritmo e Estrutura de Dados 2026S1
+Nome        : Gustavo Lima Guimarães
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1080
+Data        : 20/08/2026
+Objetivo    : Leia 100 valores inteiros. Apresente então o maior valor lido e a posição dentre os 100 valores lidos com alocação dinâmica de vetores.
+Dificuldade : Passar para a função o vetor.
+Uso de IA   : Não usei.
+------------------------------------------------------------------------- */
+#include <stdio.h>
+#include <stdlib.h>
 
+void func (int *v, int n){
+    int maior, posicao, i;
+    maior = v[0];
+    posicao = 1;
+    for (i=0 ; i<n ; i++){
+        if (v[i] > maior){
+            maior = v[i];
+            posicao = i + 1;
+        }
+    }
+    printf ("O maior valor = %d e esta na posicao %d", maior, posicao);
+}
+
+int main (){
+    int *v;
+    printf ("Digite o tamanho : ");
+    int n, i;
+    scanf ("%d", &n);
+    // Aqui vou selecionar espaço da memoria para poder caber o vetor
+    v = (int * ) malloc(n * sizeof(int));
+    // verificar se o espaço que eu reservei está vazio 
+    if (v == NULL){
+        printf ("Erro de alocação\n");
+        return 1;
+    }
+    printf ("Digite os valores : ");
+    for (int i = 0; i < n; i++) {
+    scanf("%d", &v[i]);
+    }
+    func (v, n);
+    return 0;
+}
